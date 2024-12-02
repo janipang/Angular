da0ffd1d24f182fdd704151cfb5208cbee6c3f89
janipang <pang.janista@gmail.com>
2024-12-02 22:29:27 +0700

    learn:style binding

--- src/app/app-card/app-card.component.ts
+++ src/app/app-card/app-card.component.ts
@@ -1,13 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { MotorbikeProfile } from '../motorbike-profile';
+import { EngineSizeEnum } from '../engine-size-enum';
 
 @Component({
   selector: 'app-card',
   standalone: true,
+  imports: [],
   templateUrl: './app-card.component.html',
   styleUrl: './app-card.component.css'
 })
 export class AppCardComponent {
   @Input() profile!:MotorbikeProfile; //camel case for props name
   favorited: boolean = true;
+  EngineSizeEnum = EngineSizeEnum;
 }
