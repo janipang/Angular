33d002303b0e37449aa991d81d8723d3b9e7999a
janipang <pang.janista@gmail.com>
2024-12-06 12:07:40 +0700

    learn: input event binding

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,8 +5,16 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class AppNavbarComponent {
-
+  keyEnter(event: any) {
+    console.log(event.key);
+    if (event.keyCode == 13) {
+      console.log('Are you sure to submit!');
+    }
+  }
+  handleSubmit(){
+    alert("Are you sure to submit!")
+  }
 }
