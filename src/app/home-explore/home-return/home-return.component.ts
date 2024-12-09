3dcb8b5e2374d218fd2f8998e7b4bea010301c97
janipang <pang.janista@gmail.com>
2024-12-09 17:28:16 +0700

    learn: another way to loop

--- src/app/home-explore/home-return/home-return.component.ts
+++ src/app/home-explore/home-return/home-return.component.ts
@@ -1,14 +1,14 @@
-import { NgIf } from '@angular/common';
+import { NgIf, NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-return',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, NgTemplateOutlet],
   templateUrl: './home-return.component.html',
   styleUrl: './home-return.component.css',
 })
 export class HomeReturnComponent {
   inReturnPeriod: boolean = true
-
+  membershipLevel = "platinum" // diamond platinum gold silver copper
 }
