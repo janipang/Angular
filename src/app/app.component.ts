054e319604ee03e8ee58651eb96454b737504993
janipang <pang.janista@gmail.com>
2024-12-13 21:50:45 +0700

    learn: viewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeExploreComponent } from './home-explore/home-explore.component';
 import { AppNavbarComponent } from './navbar/navbar.component';
-import { HomeFavoriteComponent } from "./home-favorite/home-favorite.component";
+import { HomeFavoriteComponent } from './home-favorite/home-favorite.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppNavbarComponent, HomeExploreComponent, HomeFavoriteComponent],
+  imports: [
+    RouterOutlet,
+    AppNavbarComponent,
+    HomeExploreComponent,
+    HomeFavoriteComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'MotoBuddy';
+  contents: string[] = [];
+  @ViewChild(HomeExploreComponent) explore!: HomeExploreComponent;
+  @ViewChild(HomeFavoriteComponent) favorite!: HomeFavoriteComponent;
+
+  ngAfterViewInit() {
+    this.contents.push(this.explore.title);
+    this.contents.push(this.favorite.title);
+
+    console.log(this.contents);
+  }
 }
